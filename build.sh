393a2c660608d84436f59a1bb48ff6696295da4a
Barrett K. Harber <barrett.harber@gmail.com>
2016-12-28 04:26:52 -0500

    Remove travis preinstalled packages and cpu arch

--- build.sh
+++ build.sh
@@ -4,28 +4,18 @@ export ROOT=`pwd`
 
 sudo apt-get update
 sudo apt-get -y install \
-	autoconf \
-	automake \
-	build-essential \
-	cmake \
 	libass-dev \
 	libfdk-aac-dev \
-	libfreetype6-dev \
 	libmp3lame-dev \
 	libopus-dev \
 	libtheora-dev \
-	libtool \
 	libvorbis-dev \
 	libx264-dev \
-	mercurial \
-	pkg-config \
 	texinfo \
-	yasm \
-	zlib1g-dev
-
-git submodule init
+	yasm
 
 # libx265
+echo "Building libx265"
 cd $ROOT/x265/build/linux
 PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source
 make
@@ -33,12 +23,14 @@ make install
 make distclean
 
 # libvpx
+echo "Building libvpx"
 cd $ROOT/libvpx
 PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --disable-examples --disable-unit-tests
 PATH="$HOME/bin:$PATH" make
 make install
 make clean
 
+echo "Building ffmpeg"
 cd $ROOT/ffmpeg
 PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure \
 	--prefix="$HOME/ffmpeg_build" \
@@ -63,11 +55,9 @@ PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./conf
 	--enable-libx264 \
 	--enable-libx265 \
 	--enable-libxvid \
-	--arch=x86_64 \
 	--disable-cuda \
 	--disable-cuvid \
-	--disable-nvenc \
-	--cpu=i686
+	--disable-nvenc
 PATH="$HOME/bin:$PATH" make
 make install
 make distclean
