dac1dc2ee898addf6a2cd55b8333f77ba5180c64
Barrett K. Harber <barrett.harber@gmail.com>
2016-12-31 19:37:09 -0500

    fixing git push in cron [cron debug]

--- build.sh
+++ build.sh
@@ -3,75 +3,70 @@
 set -e
 ROOT=`pwd`
 
-
 if [[ "$TRAVIS_EVENT_TYPE" == 'cron' ]] || [[ `git --no-pager log -1 --oneline` == *'[cron debug]'* ]]; then
 	REPO=`git config remote.origin.url`
 	SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+
 	chmod 600 deploy_key
 	eval `ssh-agent -s`
 	ssh-add deploy_key
 
 	TODAY=`date -u +%F`
 
+	git fetch --tags
 	git submodule foreach 'git checkout master && git fetch origin master --tags && git pull'
 
 	if [[ -n `git status -s` ]]; then
 		git commit -am "Update to latest master on all for $TODAY"
 	fi
 
+	# Gather tags latest non-nightly tags for the main repo and FFmpeg submodule
+	MAIN_LATEST_TAG=`git tag --sort=-taggerdate | grep -v "nightly*" | head -1`
+	cd ffmpeg
+	FFMPEG_LATEST_TAG=`git tag --sort=-taggerdate | head -1`
+	cd ..
+
 	# Tag the nightly release
 	git tag -a "nightly-$TODAY" -m "Nightly build for $TODAY"
 
-	function latest_tag() {
-		git describe --tags "`git rev-list HEAD --not --tags='nightly*' --max-count=1`"
-	}
-
-	# If the FFmpeg repo has a newer tag, create a tag for this repo and push
-	git fetch $SSH_REPO --tags
-	MAIN_LATEST_TAG=`latest_tag`
-
-	cd $ROOT/ffmpeg
-	git FFMPEG_LATEST_TAG=`latest_tag`
-
-	cd $ROOT
-
-	if [ $MAIN_LATEST_TAG != $FFMPEG_LATEST_TAG ]; then
-		git checkout -b release/$FFMPEG_LATEST_TAG
-		git submodule foreach 'git checkout $(git describe --tags $(git rev-list --tags --max-count=1))'
-		MSG="Release for FFmpeg $FFMPEG_LATEST_TAG"$'\n'`git submodule foreach --quiet 'echo "$name: $(git describe --tags)"'`
+	# If the FFmpeg repo has a newer tag, create a tag for this repo
+	if [ "$MAIN_LATEST_TAG" != "$FFMPEG_LATEST_TAG" ]; then
+		git checkout -b "release/$FFMPEG_LATEST_TAG"
+		git submodule foreach 'git checkout $(git tag --sort=-taggerdate | head -1)'
+		MSG="Release for FFmpeg $FFMPEG_LATEST_TAG"$'\n'`git submodule foreach --quiet 'echo "$name: $(git describe --tags --abbrev=0)"'`
 		git commit -am "${MSG[@]}"
-		git tag -a $FFMPEG_LATEST_TAG -m "${MSG[@]}"
+		git tag -a "$FFMPEG_LATEST_TAG" -m "${MSG[@]}"
 	fi
 
-	git push $SSH_REPO master --tags
+	# Push updated repo and tags
+	git push "$SSH_REPO" master --tags
 
-	exit 0
+	exit 0;
 fi
 
 # Don't build master, since it will only contain the latest nightly commits.
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-	echo "Just keeping things up-to-date"
+	echo "Skipping build for 'master' branch... Just keeping things up-to-date"
 	exit 0;
 fi
 
 # libx265
 echo "Building libx265"
-cd $ROOT/x265/build/linux
+cd "$ROOT/x265/build/linux"
 PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source
 make
 make install
-make distclean
 
 # libvpx
 echo "Building libvpx"
-cd $ROOT/libvpx
+cd "$ROOT/libvpx"
 PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --disable-examples --disable-unit-tests
 PATH="$HOME/bin:$PATH" make
 make install
 make clean
 
 echo "Building FFmpeg"
-cd $ROOT/ffmpeg
+cd "$ROOT/ffmpeg"
 PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure \
 	--prefix="$HOME/ffmpeg_build" \
 	--pkg-config-flags="--static" \
@@ -89,7 +84,6 @@ PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./conf
 	--enable-protocol=file \
 	--enable-protocol=pipe \
 	--enable-protocol=tee \
-	--enable-libfdk-aac \
 	--enable-libmp3lame \
 	--enable-libvpx \
 	--enable-libx264 \
@@ -111,6 +105,6 @@ Build date: `date -u`
 `$HOME/bin/ffmpeg -version`
 
 This build of FFmpeg is licensed under the GPL. See the source code for details.
-" > $HOME/bin/README.txt
+" > "$HOME/bin/README.txt"
 
-tar -czvf $TRAVIS_BUILD_DIR/ffmpeg-build-lambda.tar.gz $HOME/bin/*
+tar -czvf "$TRAVIS_BUILD_DIR/ffmpeg-build-lambda.tar.gz" $HOME/bin/*
