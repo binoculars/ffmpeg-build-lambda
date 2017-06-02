0d9c99f4008e561780169ed73c0d12cdb4a498d5
alexxty7 <alexxty7@gmail.com>
2017-06-02 17:44:33 +0300

    Change build script

--- build.sh
+++ build.sh
@@ -4,6 +4,50 @@ set -ev
 ROOT=`pwd`
 NUM_CORES=2
 
+if [[ "$TRAVIS_EVENT_TYPE" == 'cron' ]] || [[ `git --no-pager log -1 --oneline` == *'[cron debug]'* ]]; then
+	REPO=`git config remote.origin.url`
+	SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+
+	chmod 600 deploy_key
+	eval `ssh-agent -s`
+	ssh-add deploy_key
+
+	TODAY=`date -u +%F`
+
+	git fetch --tags
+	git submodule foreach 'git checkout master && git fetch origin master --tags && git pull'
+
+	if [[ -n `git status -s` ]]; then
+		git commit -am "Update to latest master on all for $TODAY"
+	fi
+
+	# Gather tags latest non-nightly tags for the main repo and FFmpeg submodule
+	MAIN_LATEST_TAG=`git tag | xargs -I@ git log --format=format:"%ai @%n" -1 @ | sort -r | awk '{print $4}' | grep -v "nightly*" | head -1`
+	cd ffmpeg
+	FFMPEG_LATEST_TAG=`git tag | xargs -I@ git log --format=format:"%ai @%n" -1 @ | sort -r | awk '{print $4}' | head -1`
+	cd ..
+
+	# Tag the nightly release
+	TAG_NAME_TODAY="nightly-$TODAY"
+	if ! git rev-parse -q --verify "refs/tags/$TAG_NAME_TODAY" >/dev/null; then
+		git tag -a "$TAG_NAME_TODAY" -m "Nightly build for $TODAY"
+	fi
+
+	# If the FFmpeg repo has a newer tag, create a tag for this repo
+	if [ "$MAIN_LATEST_TAG" != "$FFMPEG_LATEST_TAG" ] && ! git rev-parse -q --verify "refs/tags/$MAIN_LATEST_TAG" >/dev/null; then
+		git checkout -b "release/$FFMPEG_LATEST_TAG"
+		git submodule foreach $'git checkout $(git tag | xargs -I@ git log --format=format:"%ai @%n" -1 @ | sort -r | awk \'{print $4}\' | head -1)'
+		MSG="Release for FFmpeg $FFMPEG_LATEST_TAG"$'\n'`git submodule foreach --quiet 'echo "$name: $(git describe --tags --abbrev=0)"'`
+		git commit -am "${MSG[@]}"
+		git tag -a "$FFMPEG_LATEST_TAG" -m "${MSG[@]}"
+	fi
+
+	# Push updated repo and tags
+	git push "$SSH_REPO" master --tags
+
+	exit 0;
+fi
+
 # libx265
 cd "$ROOT/x265/build/linux"
 PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source
